c8df148b5d7d3aabbbd7be30b289bcb6f7465272
areppzubaidi <your-email@example.com>
2026-05-10 17:49:44 +0800

    fix: update ubuntu AMI filter for ap-southeast-1 region

--- terraform/main.tf
+++ terraform/main.tf
@@ -31,9 +31,20 @@ data "aws_subnets" "default" {
 data "aws_ami" "ubuntu" {
   most_recent = true
   owners      = ["099720109477"]
+
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-jammy-22.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "state"
+    values = ["available"]
   }
 }
 
